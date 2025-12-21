c32d20e55040447e622791135091941aa67efee9
Samuel Worash Getaneh <samuelworash@gmail.com>
2025-12-21 20:56:10 +0300

    chore: in code documentation, code cleanup, README added, gitignore updated

---
+++ README.md
@@ -0,0 +1,27 @@
+# Piano Tiles
+
+## Grup Uyeler
+
+- Mohammed Nasim Muhtasibzada (Ogr. No. 24253810) [Github](https://github.com/nasim-01)
+- Samuel Worash Getaneh (Ogr. No. 24253809) [Github](https://github.com/kmab)
+- Santiago Ntuntu Masango Mikue (Ogr. No. 24253811) [Github](https://github.com/santi-mikue)
+
+## Oyun Tanim
+
+falan filan
+
+## Calistirmak icin gerekli adimlar
+
+falan filan
+
+1. falan 1
+
+    ```c
+    main.c calistir
+    ```
+
+2. falan 2
+
+3. falan 3
+
+### algo lab 141 icin ask ile yaptirdi 💖
--- main.c
+++ main.c
@@ -15,7 +15,7 @@
 #define EASY (float)(tileHeight / 25.0f)
 #define MEDIUM (float)(tileHeight / 20.0f)
 #define HARD (float)(tileHeight / 15.0f)
-#define HIGHTSCORE_FILE "hightscore.dat"
+#define HIGHTSCORE_FILE "highscore.dat"
 
 // TODO List
 // README :IMPORTANT
@@ -27,38 +27,6 @@
 // Add notes to tiles
 // Avatar in the corner shouting when u score a point
 // Song guided tile generation
-
-// HIGHTSCORE
-//1- to load
-int loadHightscore(void){
- FILE *file;
-int hightscore = 0;
-file = fopen (HIGHTSCORE_FILE, "r");
-if(file != NULL){
-    fscanf(file, "%d", &hightscore);
-    fclose(file);
-}
-return hightscore;
-}
-//to save
-void saveHightscore(int hightscore){
-     FILE *file;
-file = fopen (HIGHTSCORE_FILE, "w");
-if(file != NULL){
-    fprintf(file, "%d", hightscore);
-    fclose(file);
-}
-}
-
-//to update
-int updateHigthscore(int score, int hightscore){
-    if(score > hightscore){
-        hightscore = score;
-        saveHightscore(hightscore);
-    }
-    return hightscore;
-}
-
 
 // Structs
 // Tile | column - 0, 1, 2, 3 | y - y coordinate | active - true/false
@@ -78,7 +46,7 @@ typedef enum Round {
     ROUND_1 = 50, ROUND_2 = 100
 } Round;
 
-// Game instance
+// Game instance | score (int) | highscore (int) | theLine (float - y coord) | tiles (Tiles[]) | speed (float - EASY/MEDIUM/HARD) | generated (int) | scored (int) | gameState (PLAYING/PAUSE/END)
 typedef struct Game {
     int score;
     int highscore;
@@ -96,29 +64,59 @@ static void UpdateGame(Game *game); // Update game physics
 static void DrawFrame(Game *game); // Update and draw one frame
 static void GenerateRandomTile(Game *game, int prev); // Generate tile in one of four collumns
 static void CheckTilesOutsideScreen(Game *game); // Check if each tile is outside of the screen or near the line
+static int loadHighscore(void); // Load highscore
+static void saveHighscore(int hightscore); // Save highscore into file
+static int updateHighscore(int score, int highscore); // Update the highscore
 
 // Main entry point
 int main() {
-
+    // Window init (Perence baslat)
     InitWindow(screenWidth, screenHeight, "piano tiles");
-
-    Game game;
-
-    GameInit(&game);
-
     SetTargetFPS(60);
-
+    Game game;
+    // Init Game (Oyun Baslat)
+    GameInit(&game);    
+    // Game Loop (Oyun Dongusu)
     while (!WindowShouldClose()) {
         UpdateGame(&game);
         DrawFrame(&game);
     }
-
     CloseWindow();
-
     return 0;
 }
 
 // Fonksiyonlar
+// Load highscore
+static int loadHighscore(void) {
+    FILE *file;
+    int highscore = 0;
+    file = fopen (HIGHTSCORE_FILE, "r");
+    if(file != NULL){
+        fscanf(file, "%d", &highscore);
+        fclose(file);
+    }
+    return highscore;
+}
+
+// Save highscore
+static void saveHighscore(int highscore) {
+    FILE *file;
+    file = fopen (HIGHTSCORE_FILE, "w");
+    if(file != NULL){
+        fprintf(file, "%d", highscore);
+        fclose(file);
+    }
+}
+
+// Update highscore
+static int updateHighscore(int score, int highscore) {
+    if(score > highscore){
+        highscore = score;
+        saveHighscore(highscore);
+    }
+    return highscore;
+}
+
 // Initialize game stats
 static void GameInit(Game *game) {
     game->score = 0;
@@ -135,33 +133,30 @@ static void GameInit(Game *game) {
     game->generated = 0;
     game->scored = 0;
     GenerateRandomTile(game, GetRandomValue(0, 3));
-    game->highscore = loadHightscore();
+    game->highscore = loadHighscore();
 }
 
 // Generate tile in one of four collumns
 static void GenerateRandomTile(Game *game, int prev) {
     int col = GetRandomValue(0, 3);
-    if(col == prev) col = (col + 1) % 4;
+    if(col == prev) col = (col + 1) % 4; // Ensures they don't spawn on the same column
     Tile tile = {
         col,
-        -tileHeight,
+        -tileHeight, // Spawn just outside the window
         true
     };
     for(int i = 0; i < tileNumber; i++) {
         if(!game->tiles[i].active) {
-            if((game->generated < ROUND_1 && game->speed == EASY) || (game->generated < ROUND_2 && game->speed == MEDIUM) || game->speed == HARD) {
+            if((game->generated < ROUND_1 && game->speed == EASY) || (game->generated < ROUND_2 && game->speed == MEDIUM) || game->speed == HARD) { // If the round & generated tiles align, spawn. Otherwise, it means a round is over -> wait until all tiles have been clicked before generating
                 game->tiles[i] = tile;
                 game->generated++;
                 return;
             }
         }
     }
-    if(game->speed == EASY) {
-        game->speed = MEDIUM;
-
-    }
+    // If there are no active tiles (and this function is called), increase the difficulty
+    if(game->speed == EASY) game->speed = MEDIUM;
     else game->speed = HARD;
-
 }
 
 // Check if tile is close to theLine
@@ -181,18 +176,14 @@ static void CheckTile(Game* game, int col) {
         }
     }
     if(bestIndex != -1) {
-        if(bestDistance > (float)(tileHeight) * 2.0f / 3.0f) game->score += 3;
-        else if(bestDistance > (float)(tileHeight) / 2.0f) game->score += 5;
-        else if(bestDistance > (float)(tileHeight) / 3.0f) game->score += 7;
-        else game->score += 10;
-        
-        // if(game->score > 100) game->speed = MEDIUM;
-        // if(game->score > 300) game->speed = HARD;
+        if(bestDistance < (float)(tileHeight) * 0.2f || bestDistance > (float)(tileHeight) * 0.8f) game->score += 3; // 3 pts if tile is [0, 20] & [80, 100]% close
+        else if(bestDistance < (float)(tileHeight) * 0.4f || bestDistance > (float)(tileHeight) * 0.6f) game->score += 6; // 6 pts if tile is [20, 40] & [60, 80]% close
+        else game->score += 10; // 10 pts if tile is [40 - 60]% close
 
         game->tiles[bestIndex].active = false;
         game->tiles[bestIndex].y = -tileHeight;
         game->scored++;
-    } else {
+    } else { // End game if wrong button clicked or timing is wrong
         game->gameState = END;
     }
 }
@@ -204,8 +195,8 @@ static void CheckTilesOutsideScreen(Game* game) {
             // Game over state
             if(game->tiles[i].y > screenHeight) {
                 game->gameState = END;
-                // TODO: Game over functions, high score stuff
-                game->highscore = updateHigthscore(game->score, game->highscore);
+                // Game over functions, update high score
+                game->highscore = updateHighscore(game->score, game->highscore);
             }
         }
     }
@@ -214,7 +205,7 @@ static void CheckTilesOutsideScreen(Game* game) {
 // Update game physics
 static void UpdateGame(Game *game) {
     if(game->gameState == END) {
-        if(IsKeyPressed(KEY_R)) {
+        if(IsKeyPressed(KEY_R)) { // Restart Game
             GameInit(game);
         }
     } else if(game->gameState == PLAYING) {
@@ -232,7 +223,10 @@ static void UpdateGame(Game *game) {
 
         for(int i = 0; i < tileNumber; i++) {
             if(game->tiles[i].active)  game->tiles[i].y += game->speed;
-            if(game->tiles[i].y == 0 || (game->scored == ROUND_1 && game->generated == ROUND_1) || (game->scored == ROUND_2 && game->generated == ROUND_2)) GenerateRandomTile(game, game->tiles[i].column);
+            if(game->tiles[i].y == 0 || // Generate tiles consecutively
+                (game->scored == ROUND_1 && game->generated == ROUND_1) || // Generate first tile when changing rounds and all tiles have been clicked
+                (game->scored == ROUND_2 && game->generated == ROUND_2)) 
+                GenerateRandomTile(game, game->tiles[i].column);
         }
         CheckTilesOutsideScreen(game);
     } else {
